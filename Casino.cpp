bbba7298063a761a4e1e458298f945ea563c4d29
Harsh32711 <hannu.harsh07@gmail.com>
2026-03-27 22:08:08 +0530

    changes USER Constructor

--- Casino.cpp
+++ Casino.cpp
@@ -5,132 +5,183 @@
 #include <cmath>
 using namespace std;
 mt19937 rng(random_device{}());
-class User{
-    protected:
+class User
+{
+protected:
     string name;
     int CID;
     int luck;
     float amountWon;
-    vector <bool> streak;
-    public:
-    User(string n, int c, int l, float a){
+    vector<bool> streak;
+
+public:
+    User(string n, int c, float a)
+    {
         name = n;
-        CID =c;
-        luck = l;
+        CID = c;
+        luck = 30;
         amountWon = a;
     }
-    void display() const{
-        cout<<"Name :"<<name<<"\nCID :"<<CID<<"Amount :"<<amountWon<<endl;
+    void display() const
+    {
+        cout << "Name :" << name << "\nCID :" << CID << "Amount :" << amountWon << endl;
     }
-    int getLuck() const{
+    int getLuck() const
+    {
         return luck;
     }
-    float getAmount() const{
+    float getAmount() const
+    {
         return amountWon;
     }
-    void updateAmount(float f){
+    void updateAmount(float f)
+    {
         amountWon += f;
     }
-    void updateLuck(int l){
+    void updateLuck(int l)
+    {
         luck += l;
-        if(luck>100) luck = 70;
-        if(luck<0) luck = 30;
+        if (luck > 100)
+            luck = 70;
+        if (luck < 0)
+            luck = 30;
     }
-    void updateStreak(bool a){
+    void updateStreak(bool a)
+    {
         streak.push_back(a);
         int N = streak.size();
-        if(N>=3){
-            if(streak[N-3] == 0 && streak[N-1] == 0 && streak[N-2] == 0) updateLuck(25);
+        if (N >= 3)
+        {
+            if (streak[N - 3] == 0 && streak[N - 1] == 0 && streak[N - 2] == 0)
+                updateLuck(25);
         }
-        if(N>6) streak.erase(streak.begin());
+        if (N > 6)
+            streak.erase(streak.begin());
     }
 };
-class Game{
-    protected:
+class Game
+{
+protected:
     string gameName;
-    int points,bet;
+    int points, bet;
     User *u = nullptr;
-    public:
+
+public:
     virtual void gameInfo() = 0;
-    void showGameName() const{
-        cout<<"Game Name :"<<gameName<<endl;
+    void showGameName() const
+    {
+        cout << "Game Name :" << gameName << endl;
     }
-    void showPoints() const{
-        cout<<"Points :"<<points<<endl;
+    void showPoints() const
+    {
+        cout << "Points :" << points << endl;
     }
-    void placeBet(){
-        while(true){
+    void placeBet()
+    {
+        while (true)
+        {
             int f = 1;
-            cout<<"Enter amount to bet :";
-            cin>>bet;
-            if(bet>u->getAmount()){
-                cout<<"Insufficient Balance!!!\n";
+            cout << "Enter amount to bet :";
+            cin >> bet;
+            if (bet > u->getAmount())
+            {
+                cout << "Insufficient Balance!!!\n";
                 f = 0;
             }
-            if(f) break;
+            if (f)
+                break;
         }
     }
 };
-class diceGuess : public Game{
-    uniform_int_distribution <int> dice;
-    uniform_int_distribution <int> chance_list;
-    public:
-    void gameInfo(){
-        cout<<"Dice Guessing!!!\nIn this game the computer would roll 2 dice and a user is asked for a choice between 2 to 12 if the Choice matches the user wins 50% of the bet\n";
+class diceGuess : public Game
+{
+    uniform_int_distribution<int> dice;
+    uniform_int_distribution<int> chance_list;
+
+public:
+    void gameInfo()
+    {
+        cout << "Dice Guessing!!!\nIn this game the computer would roll 2 dice and a user is asked for a choice between 2 to 12 if the Choice matches the user wins 50% of the bet\n";
     }
-    diceGuess(User &U): dice(1,6), chance_list(0,99){
+    diceGuess(User &U) : dice(1, 6), chance_list(0, 99)
+    {
         u = &U;
     }
-    void playDiceGame(){
-        while(true){
+    void playDiceGame()
+    {
+        while (true)
+        {
             placeBet();
-            int ch,F;
-            while(true){
-                bool f=1;
-                cout<<"Enter Your Guess :";
-                cin>>ch;
-                if(ch<2 || ch>12){
-                    cout<<"Invalid Choice!!!\n";
+            int ch, F;
+            while (true)
+            {
+                bool f = 1;
+                cout << "Enter Your Guess :";
+                cin >> ch;
+                if (ch < 2 || ch > 12)
+                {
+                    cout << "Invalid Choice!!!\n";
                     f = 0;
                 }
-                if(f) break;
+                if (f)
+                    break;
             }
             int roll = dice(rng) + dice(rng);
             int chance = chance_list(rng);
-            if(chance<(u->getLuck())){
-                if(roll>ch) roll--;
-                else if(roll<ch) roll++;
-                roll = max(2, min(12,roll));
+            if (chance < (u->getLuck()))
+            {
+                if (roll > ch)
+                    roll--;
+                else if (roll < ch)
+                    roll++;
+                roll = max(2, min(12, roll));
             }
-            cout<<"Computer Guess :"<<roll;
-            if(abs(ch-roll)==2){
-                cout<<"Close Guess!!!\nAmount Won = "<<(bet + bet*0.1)<<"!!!\n";
-                u->updateAmount(bet*0.1);
+            cout << "Computer Guess :" << roll << endl;
+            cout << "Your Guess  :" << ch << endl;
+            if (abs(ch - roll) == 2)
+            {
+                cout << "Close Guess!!!\nAmount Won = " << (bet + bet * 0.1) << "!!!\n";
+                u->updateAmount(bet * 0.1);
                 u->updateLuck(-20);
                 u->updateStreak(0);
             }
-            else if(abs(ch-roll)==1){
-                cout<<"Very Close!!!!\nAmount Won = "<<(bet + bet*0.15)<<"!!!\n";
-                u->updateAmount(bet*0.15);
+            else if (abs(ch - roll) == 1)
+            {
+                cout << "Very Close!!!!\nAmount Won = " << (bet + bet * 0.15) << "!!!\n";
+                u->updateAmount(bet * 0.15);
                 u->updateLuck(-35);
-                u->updateStreak(0);
+                u->updateStreak(1);
             }
-            else if(ch==roll){
-                cout<<"\n---------------\nJACKPOT!!!!!\n---------------\nAmount Won = "<<(bet + bet*0.5)<<"!!!\n";
-                u->updateAmount(bet*0.5);
+            else if (ch == roll)
+            {
+                cout << "\n---------------\nJACKPOT!!!!!\n---------------\nAmount Won = " << (bet + bet * 0.5) << "!!!\n";
+                u->updateAmount(bet * 0.5);
                 u->updateLuck(-80);
                 u->updateStreak(1);
             }
-            else{
-                cout<<"Well Tried!!!\nAmount Loss = "<<bet<<endl;
+            else
+            {
+                cout << "Well Tried!!!\nAmount Loss = " << bet << endl;
                 u->updateAmount(-bet);
                 u->updateLuck(20);
                 u->updateStreak(0);
             }
-            cout<<"Wanna Try Again ? I can sense the Luck Pumping!!! (Enter 1 for yes and 0 for no) :";
-            cin>>F;
-            if(F==0) break;
+            cout << "Wanna Try Again ? I can sense the Luck Pumping!!! (Enter 1 for yes and 0 for no) :";
+            cin >> F;
+            if (F == 0)
+                break;
         }
     }
-   
 };
+
+int main()
+{
+    User u("Harsh", 100, 30000);
+    u.display();
+    u.updateAmount(240);
+    u.display();
+    diceGuess a(u);
+    a.playDiceGame();
+    u.display();
+    return 0;
+}
